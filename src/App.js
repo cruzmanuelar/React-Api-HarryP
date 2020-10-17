2b13fbedd787c459126a318eefe6233af6966006
Manuel Cruz <cruzmanuelar@gmail.com>
2020-10-16 21:47:07 -0500

    RenderizandoDatos

--- src/App.js
+++ src/App.js
@@ -5,19 +5,12 @@ import Personajes from './componentes/Personajes';
 
 class App extends React.Component{
 
-  state = {
-    personajes : []
-  }
 
-  componentDidMount(){
-    fetch('http://hp-api.herokuapp.com/api/characters')
-    .then(respuesta => respuesta.json())
-    .then(respuesta => this.setState({personajes: respuesta}))
-  }
+
 
   render(){
     return(
-      <Personajes parame={this.state.personajes}/>
+      <Personajes/>
     )
   }
 }
--- src/componentes/Personajes.js
+++ src/componentes/Personajes.js
@@ -1,12 +1,40 @@
 import React from 'react';
+// import Imagen from './Imagen';
 
 class Personajes extends React.Component{
 
+    state = {
+        personajes : []
+      }
+
+    componentDidMount(){
+
+        // this.setState({personajes:["Manuel"]})
+    
+        fetch('http://hp-api.herokuapp.com/api/characters')
+        .then(respuesta => respuesta.json())
+        .then(respuesta => this.setState({personajes: respuesta}))
+    }
+
+    mostrarPersonajes = () => {
+
+        if(this.state.personajes.length === 0) return null;
+        
+        return(
+
+            <div>
+                {this.state.personajes.map(person => (
+                        console.log(person.name)
+                ))}
+            </div>
+        )
+    }
+    
 
     render(){
         return(
             <div>
-                
+                {this.mostrarPersonajes()}
             </div>
         )
     }
